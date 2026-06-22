3553e3f9d9cc60e98e3737790ab9242ef8a56860
germanrp1 <german_rp@hotmail.com>
2026-06-22 10:58:16 +0200

    Actualizacion automatica

--- src/components/maroto/Project.tsx
+++ src/components/maroto/Project.tsx
@@ -1,5 +1,6 @@
 import { motion } from "motion/react";
-import facade from "@/assets/facade.jpg";
+//import facade from "@/assets/facade.jpg";
+import facade from "@/assets/Fachada.jpeg";
 
 export function Project() {
   return (
