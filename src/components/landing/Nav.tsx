b5cb0591dba15bca77215ea9b67944b64e158978
germanrp1 <german_rp@hotmail.com>
2026-07-07 23:20:43 +0200

    Actualizacion

--- src/components/landing/Nav.tsx
+++ src/components/landing/Nav.tsx
@@ -38,7 +38,7 @@ export function Nav() {
           }`}
         >
           <Logo
-            className="h-10 md:h-12 w-auto"
+            className="h-8 md:h-10 w-auto shrink-0"
             color={scrolled ? project.branding.logoColor : "#FFFFFF"}
             // color=#869F57
           />
